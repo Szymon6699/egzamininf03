373e2a6cd55ac8d7d55b2d1279db47d153076adc
Szymon6699 <113582482+Szymon6699@users.noreply.github.com>
2023-04-05 15:32:07 +0200

    1

---
+++ wedkuj.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+
+    <body>
+
+        <div class="b1">
+
+            <h1>Portal dla Wędkarzy</h1>
+
+        </div>
+
+        <div class="lbg">
+        <div class="lbg1">
+
+            <h3>Ryby zamieszkujące rzeki</h3>
+
+          <ol>
+            <li> </li>
+            <li> </li>
+          </ol>
+
+        </div>
+
+        <div class="lbg2">
+
+           <h3>Ryby drapieżne naszych wód</h3>
+
+           <table>
+
+                <tr>
+                    <td>L.p. </td> <td>Gatunek </td> <td>Występowanie </td>
+                </tr>
+
+                <tr>
+                    
+                </tr>
+
+                <tr>
+
+                </tr>
+
+           </table>
+
+        </div>
+        </div>
+
+        <div class="pbg1">
+
+            <img src="ryba1.jpg" alt="sum">
+            <br><a href="kwerendy.txt">Pobierz kwerendy</a>
+
+        </div>
+
+        <div class="s1"> 
+
+        <p>Stronę wykonał: 00000000000</p>
+
+        </div>
+
+    </body>
+
+</html>
